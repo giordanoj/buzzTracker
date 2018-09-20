8cf8fb50615a698ed7fdd0c0bd022c09923f1bfe
john giordano <johnfg1@gmail.com>
2018-09-20 19:04:41 -0400

    create empty project

---
+++ app/src/main/java/com/cs2340/team/buzztracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs2340.team.buzztracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
