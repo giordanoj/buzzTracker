a367360350cf46f24462b740c8340965a0e112b7
Jun Chen <junchen1511@gmail.com>
2018-09-23 21:43:53 -0400

    login page finish

--- app/src/main/java/com/cs2340/team/buzztracker/controllers/Login.java
+++ app/src/main/java/com/cs2340/team/buzztracker/controllers/Login.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cs2340.team.buzztracker.R;
+
+import org.w3c.dom.Text;
 
 public class Login extends Activity {
 
@@ -29,19 +33,34 @@ public class Login extends Activity {
 
         );
 
-
-
+        final EditText loginUsername = (EditText) findViewById(R.id.username);
+        final EditText loginPassword = (EditText) findViewById(R.id.passwordText);
+        final TextView badlogin = (TextView) findViewById(R.id.badLogin);
+        badlogin.setVisibility(View.INVISIBLE);
         Button loginBtn = (Button) findViewById(R.id.loginbtn1);
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent loginA = new Intent(Login.this, ApplicationAcitivity.class);
-                startActivity(loginA);
-            }}
+
+
+                @Override
+                public void onClick(View v) {
+
+                    if(loginUsername.getText().toString().equals("User") &&
+                            loginPassword.getText().toString().equals("Password")) {
+                        Intent loginA = new Intent(Login.this, ApplicationAcitivity.class);
+                        startActivity(loginA);
+                    } else {
+                        badlogin.setVisibility(View.VISIBLE);
+
+                    }
+                }}
 
 
         );
+
+
+
 
     }
 
