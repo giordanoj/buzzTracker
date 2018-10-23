40fdd5ba245b9f8e6e9ae346b7192204a03d6466
John Landers <john.landers@gatech.edu>
2018-10-22 20:22:51 -0400

    Made changes to profile page and created an Add Location page.

---
+++ app/src/main/java/com/cs2340/team/buzztracker/controllers/AddDonationActivity.java
@@ -0,0 +1,19 @@
+package com.cs2340.team.buzztracker.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.cs2340.team.buzztracker.R;
+
+public class AddDonationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+    }
+}
