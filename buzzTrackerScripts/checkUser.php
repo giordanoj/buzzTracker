92c51c2627750a994c56900ae3ad815e0a37a159
wahdo7 <ryanmarino99@gmail.com>
2018-10-24 01:26:14 -0400

    Add files via upload

--- buzzTrackerScripts/checkUser.php
+++ buzzTrackerScripts/checkUser.php
@@ -6,7 +6,7 @@ $password = "password";
 $db_name = "dt";
 $table_name = "user";
 
-$input_username = $_REQUEST["username"];
+$input_email= $_REQUEST["email"];
 $input_password = $_REQUEST["password"];
 
 // Create connection
@@ -16,14 +16,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM $table_name WHERE username = '$input_username' AND password = '$input_password'";
+$sql = "SELECT * FROM $table_name WHERE email = '$input_email' AND password = '$input_password'";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows == 1) {
     while($row = $result->fetch_assoc()) {
-        echo $row["id"] . "_" . $row["name"] . "_" . $row["accountType"];
+        echo "|Id:" . $row["id"] . ", Name:" . $row["name"] . ", Email:" . $row["email"] . ", Password:" . $row["password"] . ", Locked:" . $row["locked"] . ", Type:" . $row["type"] . ", Location:" . $row["location"] . ",";
     }
+    echo "|";
 } else {
     echo "user not found";
 }
