1eff52429609de3453461f1493b7b58360afb5ad
Jun Chen <junchen1511@gmail.com>
2018-11-08 20:15:40 -0500

    Javadoc Comments

--- app/src/main/java/com/cs2340/team/buzztracker/model/UserTypes.java
+++ app/src/main/java/com/cs2340/team/buzztracker/model/UserTypes.java
@@ -1,13 +1,35 @@
 package com.cs2340.team.buzztracker.model;
 
+/**
+Class represent the Types of Users
+
+*/
+
 public enum  UserTypes {
+    /**
+     * Type of User: Regular User
+     */
     User("User"),
+    /**
+     * Type of User: Admin
+     */
     Admin("Admin"),
+    /**
+     * Type of User: Location Employee
+     */
     Location_Employee("Location Employee"),
+    /**
+     * Type of User: Manager
+     */
     Manager("Manager");
 
 
     private final String var;
+
+    /**
+     * 
+     * @param String value representing the User Type
+     */
 
     UserTypes(String value) {
 
